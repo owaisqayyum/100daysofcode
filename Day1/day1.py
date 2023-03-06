835ae42251162f3b1c8a4fce9b4b80ba17ee7436
owaisqayyum <oqa001@uit.no>
2023-03-06 14:06:58 +0100

    day 1

---
+++ Day1/day1.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up?\n")
+
+pet = input("What's your pets name?\n")
+
+print("Your band name could be " + city + " " + pet)
---
+++ Day1/readme.md
@@ -0,0 +1,3 @@
+# Day 1 (Band Name Generator)
+
+In order to run this code, checkout this [link](https://replit.com/@owaisqayyum/Day-1-Band-Name-Genrator#main.py)
